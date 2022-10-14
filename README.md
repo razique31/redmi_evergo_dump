367f2704fdd56ff018ae60cc4a8a5340b775dada
razxd <mdrazique31@gmail.com>
2022-10-14 16:52:02 +0530

    Add extras for evergo-user 12 SP1A.210812.016 V13.0.5.0.SGBINXM release-keys
    Signed-off-by: razxd <mdrazique31@gmail.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## evergo-user 12 SP1A.210812.016 V13.0.5.0.SGBINXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6833
+- Codename: evergo
+- Brand: Redmi
+- Flavor: evergo-user
+- Release Version: 12
+- Id: SP1A.210812.016
+- Incremental: V13.0.5.0.SGBINXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Locale: en-GB
+- Screen Density: 440
+- Fingerprint: Redmi/evergo_in/evergo:12/SP1A.210812.016/V13.0.5.0.SGBINXM:user/release-keys
+- OTA version: 
+- Branch: evergo-user-12-SP1A.210812.016-V13.0.5.0.SGBINXM-release-keys
+- Repo: redmi_evergo_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cat cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk.* 2>/dev/null >> cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk
+rm -f cust/app/customized/partner-com.facebook.katana_91/partner-com.facebook.katana_91.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.spotify.music_159/partner-com.spotify.music_159.apk.* 2>/dev/null >> cust/app/customized/partner-com.spotify.music_159/partner-com.spotify.music_159.apk
+rm -f cust/app/customized/partner-com.spotify.music_159/partner-com.spotify.music_159.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk.* 2>/dev/null >> cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk
+rm -f cust/app/customized/partner-com.snapchat.android_200/partner-com.snapchat.android_200.apk.* 2>/dev/null
+cat system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system/system/system_ext/priv-app/Settings/Settings.apk
+rm -f system/system/system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system/system/system_ext/apex/com.android.vndk.v30.apex
+rm -f system/system/system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk.* 2>/dev/null >> system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk
+rm -f system/system/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null >> system/system/priv-app/SecurityCenter/SecurityCenter.apk
+rm -f system/system/priv-app/SecurityCenter/SecurityCenter.apk.* 2>/dev/null
+cat system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk.* 2>/dev/null >> system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk
+rm -f system/system/data-app/MIMediaEditorGlobal/MIMediaEditorGlobal.apk.* 2>/dev/null
+cat system/system/lib64/librelight_only_extraphoto.so.* 2>/dev/null >> system/system/lib64/librelight_only_extraphoto.so
+rm -f system/system/lib64/librelight_only_extraphoto.so.* 2>/dev/null
+cat bootimg/01_dtbdump_MT6833.dtb.* 2>/dev/null >> bootimg/01_dtbdump_MT6833.dtb
+rm -f bootimg/01_dtbdump_MT6833.dtb.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null >> product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk
+rm -f product/app/SpeechServicesByGoogle/SpeechServicesByGoogle.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/Messages/Messages.apk.* 2>/dev/null >> product/app/Messages/Messages.apk
+rm -f product/app/Messages/Messages.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null >> product/app/TrichromeLibrary/TrichromeLibrary.apk
+rm -f product/app/TrichromeLibrary/TrichromeLibrary.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/data-app/Photos/Photos.apk.* 2>/dev/null >> product/data-app/Photos/Photos.apk
+rm -f product/data-app/Photos/Photos.apk.* 2>/dev/null
